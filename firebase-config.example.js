975ebcaa69b2422a84276239c206aac5fd929e61
zavalachumpitazoscar <zavalachumpitazoscar@gmail.com>
2026-09-01 11:05:36 -0500

    Agregar firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// Copie este archivo como firebase-config.js y reemplace los valores
+// con la configuración de su proyecto en Firebase Console.
+export const firebaseConfig = {
+  apiKey: "SU_API_KEY",
+  authDomain: "SU_PROYECTO.firebaseapp.com",
+  projectId: "SU_PROYECTO",
+  storageBucket: "SU_PROYECTO.firebasestorage.app",
+  messagingSenderId: "SU_MESSAGING_SENDER_ID",
+  appId: "SU_APP_ID"
+};
